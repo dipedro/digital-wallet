7428038f9d442b6a079ee48aea4faed0b4b32f00
Pedro Azevedo <dipedrozd@hotmail.com>
2024-05-02 23:05:38 -0300

    chore(api-gateway): add class validators in Dtos

--- api-gateway/src/wallet/wallet.dto.ts
+++ api-gateway/src/wallet/wallet.dto.ts
@@ -1,4 +1,6 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsEnum, IsOptional, IsUUID, ValidateIf } from "class-validator";
+import { OperationType } from "./enums";
 
 export class FindResponseDto {
 	@ApiProperty({ example: 100 })
@@ -7,13 +9,31 @@ export class FindResponseDto {
 
 export class FindExtractResponseDto {
 	@ApiProperty({ example: 'Pedro Azevedo' })
-	customer: string | null;
+	customer?: string | null;
+
 	@ApiProperty({ example: 10 })
 	amount: number;
+
 	@ApiProperty({ example: '2024-05-01T00:00:00.000Z' })
 	date: Date;
 
-	// TODO: add transaction type
 	@ApiProperty({ example: 'deposit' })
-	type: string;
+	operationType: OperationType;
+}
+
+export class MakeTransactionRequestDto {
+	@ApiProperty({ example: 'deposit, withdraw, purchase, chargeback, cancellation' })
+	@IsEnum(OperationType)
+	operationType: OperationType;
+
+	@ApiProperty({ example: '123' })
+	@IsUUID()
+	@IsOptional()
+	@ValidateIf((o) => [OperationType.CANCELLATION , OperationType.CHARGEBACK].includes(o.operationType))
+	transactionId?: string | null;
+
+	@ApiProperty({ example: 100 })
+	@IsOptional()
+	@ValidateIf((o) => ![OperationType.CANCELLATION , OperationType.CHARGEBACK].includes(o.operationType))
+	amount?: number | null;
 }
