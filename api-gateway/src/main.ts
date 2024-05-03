05a3723fe66e19d5ff10bb0751fca2a87df80f7f
Pedro Azevedo <dipedrozd@hotmail.com>
2024-05-02 23:01:55 -0300

    chore(api-gateway): add nestjs validator and transformer

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -1,9 +1,17 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+        transform: true,
+        whitelist: true,
+    }),
+  );
 
   const config = new DocumentBuilder()
     .setTitle('Digital Wallet API')
@@ -16,4 +24,5 @@ async function bootstrap() {
   
   await app.listen(3000);
 }
+
 bootstrap();
