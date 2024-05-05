7f713b9458dc23133314e07b63155fd0644c64ca
Pedro Azevedo <dipedrozd@hotmail.com>
2024-05-05 13:42:00 -0300

    chore(extract-ms): configure kafka client to start with the application

--- extract-ms/src/main.ts
+++ extract-ms/src/main.ts
@@ -1,8 +1,30 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { Partitioners } from 'kafkajs';
 import { AppModule } from './app.module';
 
+const logger = new Logger('ExtractMicroservice');
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.KAFKA,
+		options: {
+			client: {
+				clientId: 'extract',
+				brokers: ['localhost:9092'],
+			},
+			consumer: {
+				groupId: 'extract-consumer',
+				allowAutoTopicCreation: true
+			},
+			producer: {
+				createPartitioner: Partitioners.LegacyPartitioner 
+			}
+		}
+  });
+
+  await app.listen();
+  logger.log('Extract Microservice is running');
 }
 bootstrap();
