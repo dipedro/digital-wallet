e9ecb4ce9d45d9b9d7678bbb922d5c3d4839c95e
Pedro Azevedo <dipedrozd@hotmail.com>
2024-05-05 01:19:08 -0300

    feat(wallet-ms): implement wallet repository interface with postgres

---
+++ wallet-ms/src/infra/databases/postgres/repositories/wallet.repository.ts
@@ -0,0 +1,21 @@
+import { WalletEntity } from "src/wallet/wallet.entity";
+import { IWalletRepository } from "src/wallet/wallet.repository";
+import { Pg } from "../Pg";
+
+export class WalletPgRepository implements IWalletRepository {
+	pg: Pg;
+
+	constructor() {
+		this.pg = Pg.getInstance();
+	}
+
+	async find(id: string): Promise<WalletEntity | null> {
+		const { rows } = await this.pg.query('SELECT * FROM wallets WHERE id = $1 LIMIT 1', [id]);
+		if (rows.length === 0) {
+			return null;
+		}
+
+		const { id: walletId, balance } = rows[0];
+		return new WalletEntity(walletId, balance);
+	}
+}
