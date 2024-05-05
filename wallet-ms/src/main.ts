160d4443f44763624e9063c1f07d89fc3195527b
Pedro Azevedo <dipedrozd@hotmail.com>
2024-05-05 01:14:49 -0300

    feat(wallet-ms): configure kafka microsservice in main ts file

--- wallet-ms/src/main.ts
+++ wallet-ms/src/main.ts
@@ -1,8 +1,30 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { Partitioners } from 'kafkajs';
 import { AppModule } from './app.module';
 
+const logger = new Logger('WalletMicroservice');
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.KAFKA,
+		options: {
+			client: {
+				clientId: 'wallet',
+				brokers: ['localhost:9092'],
+			},
+			consumer: {
+				groupId: 'wallet-consumer',
+				allowAutoTopicCreation: true
+			},
+			producer: {
+				createPartitioner: Partitioners.LegacyPartitioner 
+			}
+		}
+  });
+
+  await app.listen();
+  logger.log('Wallet Microservice is running');
 }
 bootstrap();
