b283aacd845f9f5e13d217aded9a0b67ef2cff83
Pedro Azevedo <dipedrozd@hotmail.com>
2024-05-06 03:24:38 -0300

    refac(api-gateway): wallet controller file

--- api-gateway/src/wallet/wallet.controller.ts
+++ api-gateway/src/wallet/wallet.controller.ts
@@ -1,10 +1,10 @@
-import { Body, Controller, Get, HttpStatus, OnModuleInit, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, NotFoundException, OnModuleInit, Param, Post } from '@nestjs/common';
 import { Client, ClientKafka, Transport } from '@nestjs/microservices';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Partitioners } from 'kafkajs';
-import { Observable } from 'rxjs';
+import { Observable, catchError } from 'rxjs';
 import { WalletEvent } from './enums';
-import { FindExtractResponseDto, FindResponseDto, MakeTransactionRequestDto } from './wallet.dto';
+import { FindExtractResponseDto, FindResponseDto, MakeTransactionRequestDto, MakeTransactionResponseDto } from './wallet.dto';
 
 @ApiTags('Wallet')
 @Controller('wallets')
@@ -45,32 +45,12 @@ export class WalletController implements OnModuleInit {
 	})
 	private clientExtract: ClientKafka;
 
-	@Client({
-		transport: Transport.KAFKA,
-		options: {
-			client: {
-				clientId: 'trasanction',
-				brokers: ['localhost:9092'],
-			},
-			consumer: {
-				groupId: 'trasanction-consumer',
-				allowAutoTopicCreation: true
-			},
-			producer: {
-				createPartitioner: Partitioners.LegacyPartitioner,
-				idempotent: true
-			}
-		}
-	})
-	private clientTransaction: ClientKafka;
-
 	async onModuleInit() {
 		this.clientWallet.subscribeToResponseOf(WalletEvent.FIND_BALANCE);
+		this.clientWallet.subscribeToResponseOf(WalletEvent.MAKE_TRANSACTION);
 		this.clientExtract.subscribeToResponseOf(WalletEvent.FIND_EXTRACT);
-		this.clientTransaction.subscribeToResponseOf(WalletEvent.MAKE_TRANSACTION);
 		await this.clientWallet.connect();
 		await this.clientExtract.connect();
-		await this.clientTransaction.connect();
 	}
 
 	@Get(':id')
@@ -78,7 +58,11 @@ export class WalletController implements OnModuleInit {
 	find(@Param('id') walletId: string): Observable<FindResponseDto> {
 		return this.clientWallet.send(WalletEvent.FIND_BALANCE, {
 			walletId,
-		});
+		}).pipe(
+			catchError((error) => {
+				throw new NotFoundException(error?.message || `Wallet not found`);
+			})
+		);
 	}
 	
 	@Get(':id/extract')
@@ -86,15 +70,19 @@ export class WalletController implements OnModuleInit {
 	findExtract(@Param('id') walletId: string): Observable<FindExtractResponseDto[]> {
 		return this.clientExtract.send(WalletEvent.FIND_EXTRACT, {
 			walletId,
-		});
+		}).pipe(
+			catchError((error) => {
+			  	throw new NotFoundException(error?.message || `Wallet not found`);
+			})
+		);
 	}
 
 	@Post(':id/transaction')
-	@ApiResponse({ status: HttpStatus.CREATED })
-	createTransaction(
+	@ApiResponse({ status: HttpStatus.CREATED, type: MakeTransactionResponseDto })
+	makeTransaction(
 		@Param('id') walletId: string,
 		@Body() makeTransactionRequestDto: MakeTransactionRequestDto,
-	): Observable<void> {
+	): Observable<MakeTransactionResponseDto> {
 		return this.clientWallet.send(WalletEvent.MAKE_TRANSACTION, {
 			walletId,
 			...makeTransactionRequestDto
