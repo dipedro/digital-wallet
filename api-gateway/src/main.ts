1266600193b9e3a60ea162781ce6d23078bb29c0
Pedro Azevedo <dipedrozd@hotmail.com>
2024-05-06 03:23:19 -0300

    feat(api-gateway): enable global versioning

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -1,4 +1,4 @@
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
@@ -12,6 +12,11 @@ async function bootstrap() {
         whitelist: true,
     }),
   );
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  })
 
   const config = new DocumentBuilder()
     .setTitle('Digital Wallet API')
