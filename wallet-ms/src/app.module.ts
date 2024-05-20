d3e1a5466835985b086304950a37e7518c9bf010
Pedro Azevedo <dipedrozd@hotmail.com>
2024-05-20 13:19:55 -0300

    chore(wallet-ms): import db module inside global module

--- wallet-ms/src/app.module.ts
+++ wallet-ms/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { DBModule } from './infra/databases/db.module';
 import { TransactionModule } from './transaction/transaction.module';
 import { WalletModule } from './wallet/wallet.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), WalletModule, TransactionModule],
+  imports: [
+    ConfigModule.forRoot(), 
+    WalletModule, 
+    TransactionModule,
+    DBModule
+  ],
   controllers: [],
   providers: [],
 })
